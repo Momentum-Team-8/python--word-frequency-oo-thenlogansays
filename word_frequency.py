6a8879d085188b27cb1c19d2c3826aaf11077108
loganatkinson <logan.atkinson@gmail.com>
2021-06-15 08:11:28 -0400

    added word list class cleaning the text and putting it in a dict

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,5 @@
+import string
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has',
     'he', 'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to',
@@ -15,13 +17,41 @@ class FileReader:
         and return them as one string.
         """
         self.open = open(self.file)
-        self.read = self.text.read
+        self.read = self.open.read
         return self.read
         print(f"{self.read}")
         #raise NotImplementedError("FileReader.read_contents")
 
-FileReader("one-today.txt")
+class WordList:
+    def __init__(self, text):
+        self.text = text.split().lower()
+        self.word_list = []
+        self.dict = {}
 
+    def extract_words(self):
+        """
+        This should get all words from the text. This method
+        is responsible for lowercasing all words and stripping
+        them of punctuation.
+        """
+        for word in self.text:
+            clean = word.strip(string.punctuation)
+            self.word_list.append(cleaned)
+        return self.word_list
+
+        def remove_stop_words(self):
+        """
+        Removes all stop words from our word list. Expected to
+        be run after extract_words.
+        """
+            for word in self.word_list:
+                if word in STOP_WORDS:
+                    self.word_list.remove(word)
+                elif word in self.dict:
+                    self.dict[word] = self.dict[word] + 1
+                else word not in self.dict:
+                    self.dict[word] = 1
+        
 
 if __name__ == "__main__":
     import argparse
